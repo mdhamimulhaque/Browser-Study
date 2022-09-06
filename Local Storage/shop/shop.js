44516d5c6c356b6814bcdbc077381f6c7736030b
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-06 15:52:46 +0600

    local storage functionality done

--- Local Storage/shop/shop.js
+++ Local Storage/shop/shop.js
@@ -13,12 +13,47 @@ const addProduct = () => {
     const productName = getInputValue('product_name');
     const productPrice = getInputValue('product_price');
     showProducts(productName, productPrice)
+
+    //! --->save local storage ---
+    set_LocalStorageData(productName, productPrice)
 }
 
-// ---> display product
+// !==========> get data from local storage ===========
+const get_LocalStorageData = () => {
+    let data = localStorage.getItem('cart');
+    let cart = {};
+    if (data) {
+        cart = JSON.parse(data)
+    }
+    return cart;
+}
+// !==========> set data from local storage ===========
+const set_LocalStorageData = (productName, productPrice) => {
+    const cart = get_LocalStorageData();
+    //  ---> add product to the object as property
+    cart[productName] = productPrice;
+    const cartStringify = JSON.stringify(cart)
+    // --->set data to local storage
+    localStorage.setItem('cart', cartStringify)
+}
+
+// ---> show product ui
 const showProducts = (productName, productPrice) => {
     const itemsWrapper = document.getElementById('items_wrapper');
     const li = document.createElement('li');
     li.innerText = `${productName} : ${productPrice}`;
     itemsWrapper.appendChild(li);
 }
+
+
+//! ==========> show product data(value) from local storage <==========
+
+const transferDataFromLocalStorage = () => {
+    const cart = get_LocalStorageData();
+    for (const product in cart) {
+        const price = cart[product];
+        showProducts(product, price)
+    }
+}
+
+transferDataFromLocalStorage()
